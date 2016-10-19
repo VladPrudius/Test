ef3c86d7a1c7fd0db765092e4bb9c8cec388678b
trey tao <trey tao>
2016-10-19 13:34:00 +0300

    First commit

---
+++ src/JavaTestClass.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+/**
+ * Created by Влад on 17.10.2016.
+ */
+public class JavaTestClass {
+    public static void main(String [] args){
+        System.out.println("Hi guild. This time:");
+        System.out.print(new Date());
+    }
+}
